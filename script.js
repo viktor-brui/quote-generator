2596207779634edf3b6b86e277c42e9bb98d29df
viktor <viktor-brui@mail.ru>
2020-08-29 21:00:16 +0300

    change quotes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 // get Quote From api
 async function getQuote() {
   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
@@ -6,10 +12,19 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    if (data.quoteText.lemght > 50) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     getQuote();
-    console.log("whooops, no quote!", error);
   }
 }
 //onLoad
